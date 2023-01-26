7680fdb1e57ec90dfb3969ad5f597ca969c64d65
Emre Ilbay <12201630@student.pxl.be>
2023-01-26 23:57:00 +0100

    Finished extra info, tried to add functionality with java

---
+++ index-java.js
@@ -0,0 +1,12 @@
+function ShowExtraInfo() {
+    var window = document.getElementById("moji-extra-info-window");
+    // var state = window.style.visibility;
+    window.style.visibility = "visible";
+
+    // if (window.style.visibility === "none"){
+    //     window.style.visibility = "visible";
+    // }
+    // else{
+    //     window.style.visibility = "hidden";
+    // }
+}
