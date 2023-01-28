9a98cd5dd4501912811300b9a2e9a021d289137d
Emre Ilbay <12201630@student.pxl.be>
2023-01-28 22:42:16 +0100

    Fixed bug for moji extra info
    The pressed kanji is no longer visible when extra info window opens. Also when closing, the bug where some controls would stay has also been resolved

--- index-java.js
+++ index-java.js
@@ -2,22 +2,41 @@ function ShowExtraInfo() {
     //Get elements
     let window = document.getElementById("moji-extra-info-window");
     let container = document.getElementById("moji-extra-info-container");
-    let frog = document.getElementById("frog");
     let kobayashi = document.getElementById("kobayashi");
+    let addbutton = document.getElementById("addto-button");
+    let closebutton = document.getElementById("close-button");
+    let radical1 = document.getElementById("moji-radical1");
+    let radical2 = document.getElementById("moji-radical2");
+    let radical3 = document.getElementById("moji-radical3");
+    let kanji1 = document.getElementById("kanji1");
 
     if (window.style.visibility === "hidden"){
         //Show window
         window.style.visibility = "visible";
 
+
+        //Reset transition to default values
+        addbutton.style.transition = "200ms";
+        closebutton.style.transition = "200ms";
+        radical1.style.transition = "200ms";
+        radical2.style.transition = "200ms";
+        radical3.style.transition = "200ms";
+
         //Play animation
         window.style.animation = "fadein 350ms linear";
         container.style.animation = "extraInfoFlyIn 350ms ease-in-out";
-        frog.style.animation = "extraInfoFlyIn 350ms ease-in-out";
         kobayashi.style.animation = "extraInfoFlyIn 350ms ease-in-out";
     }
     else{
         //Play animation
         window.style.animation = "fadein 500ms ease-in reverse forwards";
+
+        //Make transition zero
+        addbutton.style.transition = "none";
+        closebutton.style.transition = "none";
+        radical1.style.transition = "none";
+        radical2.style.transition = "none";
+        radical3.style.transition = "none";
 
         //Hide window
         window.style.visibility = "hidden";
@@ -25,8 +44,17 @@ function ShowExtraInfo() {
         //Reset animation
         window.style.animation = "none";
         container.style.animation = "none";
-        frog.style.animation = "none";
         kobayashi.style.animation = "none";
+    }
+
+    if (window.style.visibility === "visible"){
+        //Hide original button
+        kanji1.style.transition = "none";
+        kanji1.style.visibility = "hidden";
+    } else{
+        //Show original button
+        kanji1.style.visibility = "visible";
+        kanji1.style.transition = "300ms";
     }
 }
 
