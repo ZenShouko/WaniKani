3b8004f36127164c53d75bbb9d9131086d79adfc
Emre Ilbay <12201630@student.pxl.be>
2023-01-28 01:48:33 +0100

    Moji-extra-info fully operational
    Clicking on the first kanji will display this window. Additionally, added a few notes for custom study. Clickable buttons now have a cursors: help.

--- index-java.js
+++ index-java.js
@@ -1,12 +1,36 @@
 function ShowExtraInfo() {
-    var window = document.getElementById("moji-extra-info-window");
-    // var state = window.style.visibility;
-    window.style.visibility = "visible";
+    //Get elements
+    const window = document.getElementById("moji-extra-info-window");
+    const container = document.getElementById("moji-extra-info-container");
 
-    // if (window.style.visibility === "none"){
-    //     window.style.visibility = "visible";
-    // }
-    // else{
-    //     window.style.visibility = "hidden";
-    // }
+    if (window.style.visibility === "hidden"){
+        //Show window
+        window.style.visibility = "visible";
+
+        //Play animation
+        window.style.animation = "fadein 500ms ease";
+        container.style.animation = "extraInfoFlyIn 300ms ease-in";
+    }
+    else{
+        //Play animation
+        window.style.animation = "fadein 500ms ease-in reverse forwards";
+
+        //Hide window after animation
+        window.style.visibility = "hidden";
+        //Reset animation
+        window.style.animation = "none";
+        container.style.animation = "none";
+    }
+}
+
+function addto_buttonalert(){
+    alert("From this button, you can add this Kanji to a custom list(s). \n" +
+        "These can be practiced later under [Extra study] => [Custom Review...]. \n" +
+        "In the navbar, you have a star that redirects you to your custom list(s).");
+}
+
+function customreview_alert(){
+    alert("Customize your review sessions. You can pick any radical/kanji/vocab of your previously unlocked levels.\n" +
+        "Categorize them. You have lots of options to filter out items. (Practice by level or class 'apprentice, guru etc'.)\n" +
+        "You can also create custom lists and pick out individual items. All up to you!");
 }
