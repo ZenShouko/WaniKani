d88df9b79045ec87893f54b2fbd15e4fb12b0280
Emre Ilbay <12201630@student.pxl.be>
2023-01-29 15:54:47 +0100

    Finished all extra info windows
    Fixed bugs where first click didn't work. Only thing left to do is to add an animation when closing the windows.

--- index-java.js
+++ index-java.js
@@ -1,19 +1,29 @@
 function ShowExtraInfo() {
     //Get elements
-    let window = document.getElementById("moji-extra-info-window");
+    //Windows
+    let darkScreen = document.getElementById("dark-screen");
+    let darkScreenVisibility = window.getComputedStyle(darkScreen).getPropertyValue("visibility");
     let container = document.getElementById("moji-extra-info-container");
+
+    //Image
     let kobayashi = document.getElementById("kobayashi");
+
+    //buttons
     let addbutton = document.getElementById("addto-button");
     let closebutton = document.getElementById("close-button");
+    let kanji1 = document.getElementById("kanji1");
+
+    //container elements
     let radical1 = document.getElementById("moji-radical1");
     let radical2 = document.getElementById("moji-radical2");
     let radical3 = document.getElementById("moji-radical3");
-    let kanji1 = document.getElementById("kanji1");
 
-    if (window.style.visibility === "hidden"){
-        //Show window
-        window.style.visibility = "visible";
-
+    //Show window
+    if (darkScreenVisibility === "hidden"){
+        //Show darkScreen
+        darkScreen.style.visibility = "visible";
+        container.style.visibility = "visible";
+        container.style.display = "grid";
 
         //Reset transition to default values
         addbutton.style.transition = "200ms";
@@ -23,13 +33,13 @@ function ShowExtraInfo() {
         radical3.style.transition = "200ms";
 
         //Play animation
-        window.style.animation = "fadein 350ms linear";
+        darkScreen.style.animation = "fadein 350ms linear";
         container.style.animation = "extraInfoFlyIn 350ms ease-in-out";
         kobayashi.style.animation = "extraInfoFlyIn 350ms ease-in-out";
     }
     else{
-        //Play animation
-        window.style.animation = "fadein 500ms ease-in reverse forwards";
+        //Play animation [doesn't work]
+        darkScreen.style.animation = "fadein 500ms ease-in reverse forwards";
 
         //Make transition zero
         addbutton.style.transition = "none";
@@ -38,23 +48,61 @@ function ShowExtraInfo() {
         radical2.style.transition = "none";
         radical3.style.transition = "none";
 
-        //Hide window
-        window.style.visibility = "hidden";
+        //Hide darkScreen
+        darkScreen.style.visibility = "hidden";
+        container.style.visibility = "hidden";
+        container.style.display = "none";
 
         //Reset animation
-        window.style.animation = "none";
+        darkScreen.style.animation = "none";
         container.style.animation = "none";
         kobayashi.style.animation = "none";
     }
 
-    if (window.style.visibility === "visible"){
+    //Hide button [Without this, the hovered button stays on top until mouse leave]
+    if (darkScreen.style.visibility === "visible"){
         //Hide original button
-        kanji1.style.transition = "none";
-        kanji1.style.visibility = "hidden";
-    } else{
+        kanji1.style.pointerEvents = "none";
+    }
+    else{
         //Show original button
-        kanji1.style.visibility = "visible";
-        kanji1.style.transition = "300ms";
+        kanji1.style.pointerEvents = "visible";
+    }
+}
+
+function ShowSrsInfo(){
+    let darkScreen = document.getElementById("dark-screen");
+    let darkScreenVisibility = getComputedStyle(darkScreen).getPropertyValue("visibility");
+    let window = document.getElementById("srs-extra-info-window");
+    let kobayashi = document.getElementById("kobayashi");
+    let buttonBlock = document.getElementById("apprenticeBlock");
+
+    //Show window
+    if (darkScreenVisibility === "hidden"){
+        darkScreen.style.visibility = "visible";
+        window.style.visibility = "visible";
+        window.style.display = "grid";
+
+        //Play animation
+        darkScreen.style.animation = "fadein 350ms linear";
+        window.style.animation = "extraInfoFlyIn 350ms ease-in-out";
+        kobayashi.style.animation = "extraInfoFlyIn 350ms ease-in-out";
+
+        //Disable button effects
+        buttonBlock.style.pointerEvents = "none";
+    }
+    else{
+        darkScreen.style.visibility = "hidden";
+        window.style.visibility = "hidden";
+        window.style.display = "none";
+
+        //enable button effect
+        buttonBlock.style.pointerEvents = "visible";
+
+        //Reset animation
+        darkScreen.style.animation = "none";
+        window.style.animation = "none";
+        kobayashi.style.animation = "none";
     }
 }
 
